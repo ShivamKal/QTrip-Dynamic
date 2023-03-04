aab03c6a6a975d5dcd663bbe68639735a918bce8
CrioUser <crio-user@criodo.com>
2023-03-05 00:00:05 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURE_DETAILS

--- frontend/modules/adventure_details_page.js
+++ frontend/modules/adventure_details_page.js
@@ -4,33 +4,81 @@ import config from "../conf/index.js";
 function getAdventureIdFromURL(search) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Get the Adventure Id from the URL
-
-
-  // Place holder for functionality to work in the Stubs
-  return null;
+  return search.split('=')[1];
 }
 //Implementation of fetch call with a paramterized input based on adventure ID
 async function fetchAdventureDetails(adventureId) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Fetch the details of the adventure by making an API call
-
-
-  // Place holder for functionality to work in the Stubs
-  return null;
+    let res = [];
+    try{
+      await fetch(`${config?.backendEndpoint}/adventures/detail/?adventure=${adventureId}`)
+      .then(x => x.text())
+      .then(y => res=JSON.parse(y));
+    }
+    catch(err){
+      return null;
+    }
+    return res;
 }
 
 //Implementation of DOM manipulation to add adventure details to DOM
 function addAdventureDetailsToDOM(adventure) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the details of the adventure to the HTML DOM
-
+  console.log(adventure)
+  const name = document.getElementById('adventure-name')
+  const subtitle = document.getElementById('adventure-subtitle')
+  const photoGallery = document.getElementById('photo-gallery')
+  const adventureContent = document.getElementById('adventure-content')
+  name.innerHTML = adventure?.name
+  subtitle.innerHTML = adventure?.subtitle
+  adventure?.images?.map((src)=>{
+    const imgwrapper = document.createElement('div')
+    const img = document.createElement('img')
+    img.className = "activity-card-image"
+    img.src = src
+    imgwrapper.appendChild(img)
+    photoGallery.appendChild(imgwrapper)
+  });
+  adventureContent.innerHTML = adventure?.content
 }
 
 //Implementation of bootstrap gallery component
 function addBootstrapPhotoGallery(images) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the bootstrap carousel to show the Adventure images
-
+  const photoGallery = document.getElementById('photo-gallery')
+  photoGallery.innerHTML = ''
+  photoGallery.className ="carousel slide"
+  photoGallery.dataset.bsRide = "carousel"
+  photoGallery.innerHTML = `  
+  <div class="carousel-indicators">
+  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Previous</span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span>
+</button>
+`
+  const carouselInner = document.createElement('div')
+  carouselInner.className ="carousel-inner"
+  images?.map((src, i)=>{
+    const imgwrapper = document.createElement('div')
+    const img = document.createElement('img')
+    imgwrapper.className = `carousel-item ${ i == 0 ? "active" :""}`
+    img.className ="activity-card-image"
+    img.src = src
+    imgwrapper.appendChild(img)
+    carouselInner.appendChild(imgwrapper)
+  });
+  photoGallery.appendChild(carouselInner)
 }
 
 //Implementation of conditional rendering of DOM based on availability
